668c2bb8564f1b63026f96ab0e579e4740f38151
James Meredith <jam47@st-andrews.ac.uk>
2021-03-13 23:44:36 +0000

    Join and Welcome added

--- components/join-game/JoinGame.tsx
+++ components/join-game/JoinGame.tsx
@@ -1,13 +1,27 @@
-import {SafeAreaView} from 'react-native';
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import React from 'react';
-import {Divider, Layout, Text} from '@ui-kitten/components';
+import { Button, Divider, Input, Layout, Text } from "@ui-kitten/components";
+
+const styles = StyleSheet.create({
+  h1: {
+    margin:10,
+  },
+  button: {
+    margin: 10,
+  },
+  input: {
+    width: '80%',
+  }
+});
 
 const JoinGame = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <Divider />
       <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text category="h1">Join Game</Text>
+        <Text category="h1" style={styles.h1}>Join Game</Text>
+        <Input style={styles.input} placeholder='Game ID'/>
+        <Button style={styles.button} onPress={console.log('Join Pressed!')}>Join</Button>
       </Layout>
     </SafeAreaView>
   );
--- components/welcome/WelcomePage.tsx
+++ components/welcome/WelcomePage.tsx
@@ -1,10 +1,29 @@
 import React, {useCallback} from 'react';
-import {SafeAreaView, View} from 'react-native';
-import {Button, Divider, Layout, TopNavigation} from '@ui-kitten/components';
+import { Image, SafeAreaView, StyleSheet, View } from "react-native";
+import { Button, Divider, Input, Layout, TopNavigation } from "@ui-kitten/components";
 
 interface Props {
   navigation: any;
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+  },
+  button: {
+    margin: 10,
+  },
+  input: {
+    width: '80%',
+  }
+});
 
 const WelcomePage = (props: Props) => {
   console.log('Welcome page props', props);
@@ -12,13 +31,22 @@ const WelcomePage = (props: Props) => {
   const navigateToJoinGameDetails = useCallback(() => {
     navigation.navigate('Join Game');
   }, [navigation]);
-
   return (
     <SafeAreaView style={{flex: 1}}>
       <TopNavigation title="Welcome Page" alignment="center" />
       <Divider />
-      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Button onPress={navigateToJoinGameDetails}>Go To Join Game</Button>
+      <Layout style={{flex: 1}}>
+        <View style={styles.view}>
+          <Image
+            style={styles.image}
+            source={require('../../public/images/logo-with-text-white.png')}
+          />
+          <Input style={styles.input} placeholder='Username'/>
+          <View style={{flexDirection: 'row'}}>
+            <Button style={styles.button} onPress={navigateToJoinGameDetails}>Login</Button>
+            <Button style={styles.button} onPress={navigateToJoinGameDetails}>Sign Up</Button>
+          </View>
+        </View>
       </Layout>
     </SafeAreaView>
   );
